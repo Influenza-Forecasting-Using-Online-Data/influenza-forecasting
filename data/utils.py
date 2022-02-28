51efdc4f7d441ad836349514c248280a9838d312
Radu <radu.echim.19@ucl.ac.uk>
2022-02-28 02:09:46 +0200

    Fix week range index to be in ISO weekly format

--- data/utils.py
+++ data/utils.py
@@ -10,11 +10,11 @@ def get_datetime_df(index_col_name='year week'):
     parent_path = my_path = os.path.abspath(os.path.dirname(__file__))
     df = pd.read_csv(os.path.join(parent_path, 'year_and_week_data_frame.csv'))
     datetime_df = pd.DataFrame({index_col_name: pd.to_datetime(df["year"].astype(str) + " " + df["week"].astype(str) + " 1",
-                                                         format="%Y %U %w")})
+                                                         format="%G %V %w")})
     datetime_df = pd.concat([datetime_df, df], axis=1)
     return datetime_df
     
-def get_week_range_df(index_col_name):
+def get_week_range_df(index_col_name='week range'):
     df = get_datetime_df(index_col_name)
     df.drop(df.columns[1], axis=1, inplace=True)
     df.set_index(index_col_name, inplace=True)
@@ -22,6 +22,8 @@ def get_week_range_df(index_col_name):
     return df
     
 def to_week_range(year_num, week_num):
-    return datetime.strptime(str(year_num) + " " + str(week_num) + " 1","%Y %U %w")
+    # %G %V %w format to parse iso dates available in python >3.6
+    # https://stackoverflow.com/questions/35128266/strptime-seems-to-create-wrong-date-from-week-number
+    return datetime.strptime(str(year_num) + " " + str(week_num) + " 1","%G %V %w")
 
     
